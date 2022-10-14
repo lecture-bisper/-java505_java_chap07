881cb5fe8ac3b0f8e9e98af7ae46cbfea645d146
lecture-bisper <bisper@outlook.kr>
2022-10-14 16:47:54 +0900

    1. Map컬렉션 사용하기 2. HashMap 사용하기 3. Hashtable 사용하기 4. Properties 사용하기.

---
+++ src/Member.java
@@ -0,0 +1,34 @@
+public class Member {
+    public String name;
+    public int age;
+
+    public Member(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+//    Object 타입 : 자바의 최상위 클래스
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Member) {
+            Member member = (Member) obj;
+            return member.name.equals(name) && (member.age == age);
+        }
+        else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode() + age;
+    }
+
+}
+
+
+
+
+
+
+
